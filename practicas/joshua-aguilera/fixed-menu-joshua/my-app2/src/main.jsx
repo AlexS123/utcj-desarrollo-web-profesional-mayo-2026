588a553acc6641a28b7cfb4be7e24a0f553a6eb2
Joshua <wylyberto4@gmail.com>
2026-05-20 18:26:13 -0600

    fixed menu joshua aguilera modificacion

--- practicas/joshua-aguilera/fixed-menu-joshua/my-app2/src/main.jsx
+++ practicas/joshua-aguilera/fixed-menu-joshua/my-app2/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Mypage from './assets/mypage.jsx'
+import Mypage from "./mypage.jsx";
 
 function App() {
   return <Mypage />
