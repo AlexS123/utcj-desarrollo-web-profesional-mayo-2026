408a3d286e47ac1bb23de22a08bb36a39ab57d44
pachecoaaron <aaronpachecomeza@gmail.com>
2026-07-01 18:38:43 -0600

    Bycrypt

--- practicas/PachecoAaron/server-Pacheco-Aaron/models/server.js
+++ practicas/PachecoAaron/server-Pacheco-Aaron/models/server.js
@@ -1,25 +1,86 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT || 5000;
 
-        
+        this.middlewares(); // ✅ Must go before routes()
         this.routes();
-        this.middlewares();
         this.listen();
+        this.usersDatabase();
     }
+
     middlewares() {
         this.app.use(express.static('public'));
-    }  
-    routes() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        
+    }
 
-    } 
-listen() {
-    this.app.listen(this.port, () => {
-        console.log(`Servidor corriendo en puerto ${this.port}`);
+    usersDatabase() {
+    mongoose.connect('mongodb://localhost:27017/Mayo2026_web_prof');
+    let Schema = mongoose.Schema;
+    const userSchema = new Schema({
+        user: String,
+        pass: String,
+        rol: String
     });
+    
+    this.userModel = mongoose.model('user', userSchema);
+
+
+    }
+
+
+    routes() {
+        this.app.get('/consultarUsuarios', (req, res) => {
+            res.json({
+                user: 'Aaron', // 
+                pass: '12345',
+                rol: 'admin'
+            });
+        });
+        this.app.post('/registrar', async (req, res) => {
+            let {user, pass, rol} = req.body;
+            console.log(`Usuario: ${user}, Contraseña: ${pass}, Rol: ${rol}`);
+            //cifrar contrasena
+            const saltRounds = 10;
+            bcrypt.genSalt(saltRounds, (err, salt) => {
+                if (err){
+                    return;
+                }
+
+            });
+            //Guardar en la db
+            const newUser = new this.userModel({
+                 user: user,
+                    pass: pass,
+                    rol: rol
+                
+                });
+                const savedUser = await newUser.save();
+                console.log(savedUser);
+                res.send(200);
+
+            res.status(200).json({
+                msg: 'Usuario registrado correctamente',
+                user,
+                pass,
+                rol
+            });
+        });
+        
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en puerto ${this.port}`);
+        });
+    }
 }
 
-}
 module.exports = Server;
--- practicas/PachecoAaron/server-Pacheco-Aaron/pasos.md
+++ practicas/PachecoAaron/server-Pacheco-Aaron/pasos.md
@@ -14,3 +14,10 @@ agregar archivos:
 -/models/server.js
 
 codificar en .env
+6 codificar
+6 Iniciar node app
+7. Agregar ruta consultar usuario
+Agregar base de datos para usuarios y roles en mongodb
+Instalar mongoose npm i mongoose
+cifrar contraseñas 
+instalar bcrypt: npm i bcrypt
